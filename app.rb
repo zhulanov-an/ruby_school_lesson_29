0d2ca9b8e59d5463c933cc45f182b230247a9a03
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-07 11:02:10 +0500

    remove sqlite3 connect and other

--- app.rb
+++ app.rb
@@ -5,40 +5,6 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:./database/barbershop.db"
 
-
-#examle access to db
-# def get_db 
-#   db = SQLite3::Database.new './database/leprosorium.sqlite'
-#   db.results_as_hash = true
-#   db
-# end
-
-#example set variable before POST or GET request
-# before do 
-#   @db = get_db
-# end
-
-#example congfiure db after reload page
-# configure do 
-#   get_db.execute 'CREATE TABLE IF NOT EXISTS "posts"
-#   (
-#   "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL UNIQUE,
-#   "created_at" DATETIME NOT NULL,
-#   "name_post" TEXT NOT NULL,
-#   "name_author" TEXT NOT NULL,
-#   "content" TEXT NOT NULL 
-#   )'
-
-#   get_db.execute 'CREATE TABLE IF NOT EXISTS "comments"
-#   (
-#   "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL UNIQUE,
-#   "created_at" DATETIME NOT NULL,
-#   "content" TEXT NOT NULL ,
-#   "name_author" TEXT NOT NULL ,
-#   "fk_post" INTEGER NOT NULL
-#   )'
-# end
-
 get '/' do
 	erb 'Hello, i`m clean project'
 end
