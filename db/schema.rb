adb93bda8816ce7ac334f9dd66120960fa3ed018
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-07 19:54:01 +0500

    delete double rows from Barbers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407143642) do
+ActiveRecord::Schema.define(version: 20160407145023) do
 
   create_table "Barbers", force: :cascade do |t|
     t.text     "name"
